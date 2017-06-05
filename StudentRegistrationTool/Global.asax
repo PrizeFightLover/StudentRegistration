571d1accc8e291f0994b68ebb3b954ee75c7d09e
Yves Theunissen <Yves Theunissen>
2017-06-05 18:41:34 +0200

    Add project files.

---
+++ StudentRegistrationTool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentRegistrationTool.MvcApplication" Language="C#" %>
---
+++ StudentRegistrationTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentRegistrationTool.Startup))]
+namespace StudentRegistrationTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
